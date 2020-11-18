4a905e8f7c98999526aecf418b28db5722c57676
cleanrq <qbyun130@163.com>
2020-11-18 18:17:24 +0800

    import

--- snowflake/src/main/java/com/qby/test/MainTest.java
+++ snowflake/src/main/java/com/qby/test/MainTest.java
@@ -6,6 +6,6 @@ import cn.hutool.core.util.IdUtil;
 public class MainTest {
     public static void main(String[] args) {
         Snowflake snowflake = IdUtil.getSnowflake(0, 0);
-        System.out.println(snowflake.nextId());
+
     }
 }
