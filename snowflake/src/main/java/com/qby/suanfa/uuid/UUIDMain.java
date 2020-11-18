7d25f10c93f9796684c756466b19b1c7bf129a44
cleanrq <qbyun130@163.com>
2020-11-18 16:59:35 +0800

    import

---
+++ snowflake/src/main/java/com/qby/suanfa/uuid/UUIDMain.java
@@ -0,0 +1,11 @@
+package com.qby.suanfa.uuid;
+
+import java.util.UUID;
+
+public class UUIDMain {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+
+        int a = Integer.parseInt("");
+    }
+}
