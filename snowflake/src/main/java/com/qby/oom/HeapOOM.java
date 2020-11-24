ab36f11b6a7839a33378158695c7051f4d5e5b22
cleanrq <qbyun130@163.com>
2020-11-24 14:34:25 +0800

    import

--- snowflake/src/main/java/com/qby/oom/HeapOOM.java
+++ snowflake/src/main/java/com/qby/oom/HeapOOM.java
@@ -1,4 +1,16 @@
 package com.qby.oom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeapOOM {
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
 }
