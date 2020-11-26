a863ecebfdd56d1af43065a05f4a14728199a2bc
cleanrq <qbyun130@163.com>
2020-11-26 14:52:53 +0800

    import

---
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -0,0 +1,76 @@
+package com.qby.suanfa;
+
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 将两个升序链表合并为一个新的 升序 链表并返回。
+     * 新链表是通过拼接给定的两个链表的所有节点组成的。
+     * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * <p>
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reverse-string
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param c
+     */
+    public void reverseString(char[] c) {
+        swap(0, c.length - 1, c);
+    }
+
+    private void swap(int s, int e, char[] c) {
+        if (s >= e) {
+            return;
+        }
+        char temp = c[s];
+        c[s] = c[e];
+        c[e] = temp;
+        swap(s + 1, e - 1, c);
+    }
+
+
+    public static void main(String[] args) {
+        char[] sz = {'a', 'b'};
+
+
+    }
+}
