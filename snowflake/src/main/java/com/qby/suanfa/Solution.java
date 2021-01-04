11e099eec3184d4741aa2f9d8aee2c36d26b3283
cleanrq <qbyun130@163.com>
2021-01-04 14:59:39 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -399,13 +399,34 @@ public class Solution {
         return str;
     }
 
+    public static String countAndSay2(int n) {
+        // 递归终止条件
+        if (n == 1) {
+            return "1";
+        }
+        StringBuffer res = new StringBuffer();
+        // 拿到上一层的字符串
+        String str = countAndSay2(n - 1);
+        int length = str.length();
+        // 开始指针为0
+        int start = 0;
+        // 注意这从起始条件要和下面长度统一
+        for (int i = 1; i < length + 1; i++) {
+            // 字符串最后一位直接拼接
+            if (i == length) {
+                res.append(i - start).append(str.charAt(start));
+                // 直到start位的字符串和i位的字符串不同，拼接并更新start位
+            } else if (str.charAt(i) != str.charAt(start)) {
+                res.append(i - start).append(str.charAt(start));
+                start = i;
+            }
+        }
+        return res.toString();
+    }
+
 
     public static void main(String[] args) {
-        int mid = 0 + (3 - 0) / 2;
-        System.out.println(mid);
-
-        int a = (int) 1.7;
-
-        System.out.println(a);
+        String s = countAndSay2(5);
+        System.out.println(s);
     }
 }
