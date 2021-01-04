fa0e6575f5fd9522f2c2972fe39517763f686961
cleanrq <qbyun130@163.com>
2021-01-04 17:40:52 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -425,8 +425,72 @@ public class Solution {
     }
 
 
+    /**
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出: 6
+     * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/maximum-subarray
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    /**
+     * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+     * <p>
+     * 如果不存在最后一个单词，请返回 0 。
+     * <p>
+     * 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: "Hello World"
+     * 输出: 5
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/length-of-last-word
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLastWord(String s) {
+        String t = s.trim();
+        if (t.length() == 0) {
+            return 0;
+        }
+
+        int start = t.length() - 1;
+        int end = t.length() - 1;
+        while (start >= 0 && t.charAt(start) != ' ') {
+            start--;
+        }
+
+        return (end - start);
+    }
+
+
     public static void main(String[] args) {
-        String s = countAndSay2(5);
-        System.out.println(s);
+
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        maxSubArray(nums);
     }
 }
