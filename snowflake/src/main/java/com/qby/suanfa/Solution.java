56f5482a584bb50cf7a810d9d3990ecc3f00436a
qiaobuyun <qbyun130@163.com>
2021-02-18 18:00:50 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -587,11 +587,47 @@ public class Solution {
         return ans.toString();
     }
 
+    /**
+     * 69. x 的平方根
+     * 实现 int sqrt(int x) 函数。
+     * <p>
+     * 计算并返回 x 的平方根，其中 x 是非负整数。
+     * <p>
+     * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+     *
+     * @param x
+     * @return
+     */
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = (int) Math.exp(0.5 * Math.log(x));
+        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+
+    /**
+     * 二分法
+     *
+     * @param x
+     * @return
+     */
+    public static int mySqrt2(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
-        maxSubArray(nums);
+        System.out.println(mySqrt2(8));
     }
 }
