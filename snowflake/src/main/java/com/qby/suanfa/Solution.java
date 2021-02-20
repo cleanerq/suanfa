aa072cc62b48e4a43d44b74d5d7cc853080ebc94
qiaobuyun <qbyun130@163.com>
2021-02-20 11:25:12 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -649,7 +649,7 @@ public class Solution {
      * 1.  1 阶 + 1 阶 + 1 阶
      * 2.  1 阶 + 2 阶
      * 3.  2 阶 + 1 阶
-     *
+     * <p>
      * 动态规划 滚动数组
      *
      * @param n
@@ -658,13 +658,43 @@ public class Solution {
     public int climbStairs(int n) {
         int fir, sec = 0;
         int thi = 1;
-        for (int i = 1; i<= n; i++) {
+        for (int i = 1; i <= n; i++) {
             fir = sec;
             sec = thi;
             thi = fir + sec;
         }
         return thi;
     }
+
+    /**
+     * 83. 删除排序链表中的重复元素
+     * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 1->1->2
+     * 输出: 1->2
+     * 示例 2:
+     * <p>
+     * 输入: 1->1->2->3->3
+     * 输出: 1->2->3
+     *
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                ListNode tmpNode = current.next;
+                current.next = tmpNode.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
 
     public static void main(String[] args) {
 
