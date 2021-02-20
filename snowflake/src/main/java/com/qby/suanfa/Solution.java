d631f1642a7d3c8e15b34862216ecbd3e6076a75
qiaobuyun <qbyun130@163.com>
2021-02-20 13:24:28 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -1,5 +1,6 @@
 package com.qby.suanfa;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Solution {
@@ -693,6 +694,93 @@ public class Solution {
             }
         }
         return head;
+    }
+
+
+    /**
+     * 88. 合并两个有序数组
+     * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+     * <p>
+     * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * 输出：[1,2,2,3,5,6]
+     * 示例 2：
+     * <p>
+     * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+     * 输出：[1]
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    /**
+     * 双指针
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        // Make a copy of nums1.
+        int[] nums1_copy = new int[m];
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+
+        // Two get pointers for nums1_copy and nums2.
+        int p1 = 0;
+        int p2 = 0;
+
+        // Set pointer for nums1
+        int p = 0;
+
+        // Compare elements from nums1_copy and nums2
+        // and add the smallest one into nums1.
+        while ((p1 < m) && (p2 < n))
+            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+
+        // if there are still elements to add
+        if (p1 < m)
+            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+        if (p2 < n)
+            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+    }
+
+    /**
+     * 双指针 倒序
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge3(int[] nums1, int m, int[] nums2, int n) {
+
+
+        // Two get pointers for nums1_copy and nums2.
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        // Set pointer for nums1
+        int p = m + n - 1;
+
+        // Compare elements from nums1_copy and nums2
+        // and add the smallest one into nums1.
+        while ((p1 >= 0) && (p2 >= 0))
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
     }
 
 
