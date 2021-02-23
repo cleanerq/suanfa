d92a29ab089337c3272eb6378de80b22ddf858c8
qiaobuyun <qbyun130@163.com>
2021-02-23 17:05:17 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution2.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution2.java
@@ -2,10 +2,12 @@ package com.qby.suanfa;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 import java.util.Stack;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class Solution2 {
+    Random rand = new Random();
 
     public class TreeNode {
         int val;
@@ -323,5 +325,102 @@ public class Solution2 {
         }
 
         return ans;
+    }
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+     * <p>
+     * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：nums = [-10,-3,0,5,9]
+     * 输出：[0,-3,9,-10,null,5]
+     * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+     * <p>
+     * 示例 2：
+     * <p>
+     * <p>
+     * 输入：nums = [1,3]
+     * 输出：[3,1]
+     * 解释：[1,3] 和 [3,1] 都是高度平衡二叉搜索树。
+     *
+     * 方法一：中序遍历，总是选择中间位置左边的数字作为根节点
+     *
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * 方法一：中序遍历，总是选择中间位置左边的数字作为根节点
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+    /**
+     * 方法二：中序遍历，总是选择中间位置右边的数字作为根节点
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    public TreeNode helper2(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置右边的数字作为根节点
+        int mid = (left + right + 1) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+    /**
+     * 方法三：中序遍历，选择任意一个中间位置数字作为根节点
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    public TreeNode helper3(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 选择任意一个中间位置数字作为根节点
+        int mid = (left + right + rand.nextInt(2)) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
     }
 }
