2394e19079c88d82e170fc8a7fecb216ebfe7157
qiaobuyun <qbyun130@163.com>
2021-02-24 16:26:04 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution2.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution2.java
@@ -348,7 +348,7 @@ public class Solution2 {
      * 输入：nums = [1,3]
      * 输出：[3,1]
      * 解释：[1,3] 和 [3,1] 都是高度平衡二叉搜索树。
-     *
+     * <p>
      * 方法一：中序遍历，总是选择中间位置左边的数字作为根节点
      *
      * @param nums
@@ -422,5 +422,254 @@ public class Solution2 {
         root.left = helper(nums, left, mid - 1);
         root.right = helper(nums, mid + 1, right);
         return root;
+    }
+
+    /**
+     * 110. 平衡二叉树
+     * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+     * <p>
+     * 本题中，一棵高度平衡二叉树定义为：
+     * <p>
+     * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：root = [3,9,20,null,null,15,7]
+     * 输出：true
+     * 示例 2：
+     * <p>
+     * <p>
+     * 输入：root = [1,2,2,3,3,null,null,4,4]
+     * 输出：false
+     * 示例 3：
+     * <p>
+     * 输入：root = []
+     * 输出：true
+     * 首先计算左右子树的高度，如果左右子树的高度差是否不超过 11，再分别递归地遍历左右子节点，并判断左子树和右子树是否平衡。这是一个自顶向下的递归的过程。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+
+    /**
+     * 方法二：自底向上的递归
+     * 方法一由于是自顶向下递归，因此对于同一个节点，函数 \texttt{height}height 会被重复调用，
+     * 导致时间复杂度较高。如果使用自底向上的做法，
+     * 则对于每个节点，函数 \texttt{height}height 只会被调用一次。
+     * <p>
+     * 自底向上递归的做法类似于后序遍历，对于当前遍历到的节点，先递归地判断其左右子树是否平衡，
+     * 再判断以当前节点为根的子树是否平衡。
+     * 如果一棵子树是平衡的，则返回其高度（高度一定是非负整数），否则返回 -1−1。如果存在一棵子树不平衡，
+     * 则整个二叉树一定不平衡。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/balanced-binary-tree/solution/ping-heng-er-cha-shu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced2(TreeNode root) {
+        return height2(root) >= 0;
+    }
+
+    /**
+     * 后序遍历
+     *
+     * @param root
+     * @return
+     */
+    public int height2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftH = height2(root.left);
+            int rightH = height2(root.right);
+            if (leftH == -1 || rightH == -1 || Math.abs(leftH - rightH) > 1) {
+                return -1;
+            }
+            return Math.max(leftH, rightH) + 1;
+        }
+    }
+
+    /**
+     * 111. 二叉树的最小深度
+     * 给定一个二叉树，找出其最小深度。
+     * <p>
+     * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+     * <p>
+     * 说明：叶子节点是指没有子节点的节点。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：root = [3,9,20,null,null,15,7]
+     * 输出：2
+     * 示例 2：
+     * <p>
+     * 输入：root = [2,null,3,null,4,null,5,null,6]
+     * 输出：5
+     * <p>
+     * 深度优先
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int minDepth = Integer.MAX_VALUE;
+
+        if (root.left != null) {
+            minDepth = Math.min(minDepth(root.left), minDepth);
+        }
+        if (root.right != null) {
+            minDepth = Math.min(minDepth(root.right), minDepth);
+        }
+
+        return minDepth + 1;
+    }
+
+    class QueueNode {
+        TreeNode node;
+        int depth;
+
+        public QueueNode(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int minDepth = 0;
+        Queue<QueueNode> queue = new LinkedList<>();
+        queue.offer(new QueueNode(root, 1));
+        while (!queue.isEmpty()) {
+            QueueNode node = queue.poll();
+            TreeNode treeNode = node.node;
+            if (treeNode.right == null && treeNode.left == null) {
+                return node.depth;
+            }
+
+            if (treeNode.left != null) {
+                queue.offer(new QueueNode(treeNode.left, node.depth + 1));
+            }
+            if (treeNode.right != null) {
+                queue.offer(new QueueNode(treeNode.right, node.depth + 1));
+            }
+        }
+
+        return minDepth;
+    }
+
+
+    /**
+     * 112. 路径总和
+     * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，
+     * 判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+     * <p>
+     * 叶子节点 是指没有子节点的节点。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+     * 输出：true
+     * 示例 2：
+     * <p>
+     * <p>
+     * 输入：root = [1,2,3], targetSum = 5
+     * 输出：false
+     * 示例 3：
+     * <p>
+     * 输入：root = [1,2], targetSum = 0
+     * 输出：false
+     *
+     * 深度优先 递归
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    /**
+     * 广度优先 迭代
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public boolean hasPathSum2(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        Queue<Integer> queueVal = new LinkedList<Integer>();
+        queueVal.offer(root.val);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            int temp = queueVal.poll();
+            if (node.left == null && node.right == null) {
+                if (temp == targetSum) {
+                    return true;
+                }
+                continue;
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+                queueVal.offer(temp + node.left.val);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+                queueVal.offer(temp + node.right.val);
+            }
+        }
+        return false;
     }
 }
