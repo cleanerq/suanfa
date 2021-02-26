de050b901996c1dc4e3ce5065b77533d3f028d22
qiaobuyun <qbyun130@163.com>
2021-02-26 14:43:59 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution3.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution3.java
@@ -413,8 +413,31 @@ public class Solution3 {
         return 0;
     }
 
+    /**
+     * 异或运算
+     * 如何才能做到线性时间复杂度和常数空间复杂度呢？
+     *
+     * 答案是使用位运算。对于这道题，可使用异或运算 ⊕。异或运算有以下三个性质。
+     *
+     * 任何数和 00 做异或运算，结果仍然是原来的数，即 a⊕0=a。
+     * 任何数和其自身做异或运算，结果是 00，即 a⊕a=0。
+     * 异或运算满足交换律和结合律，即 a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public static int singleNumber2(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(singleNumber(new int[]{1,2,4,1,2}));
+        System.out.println(singleNumber2(new int[]{1,2,4,1,2}));
+        System.out.println((1 ^ 3));
     }
 }
