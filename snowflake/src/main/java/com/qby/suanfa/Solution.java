d200c1682266b64135d83f3311f7d31e4db5fba2
qiaobuyun <qbyun130@163.com>
2021-03-02 18:03:17 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution.java
@@ -127,7 +127,7 @@ public class Solution {
     }
 
     public int removeDuplicates2(int[] nums) {
-        if (nums ==null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int i = 0;
@@ -838,11 +838,11 @@ public class Solution {
             if (head == null) {
                 head = tail = new ListNode(sum % 10);
             } else {
-                tail.next =  new ListNode(sum % 10);
+                tail.next = new ListNode(sum % 10);
                 tail = tail.next;
             }
 
-            carry = sum /10;
+            carry = sum / 10;
             if (l1 != null) {
                 l1 = l1.next;
             }
@@ -859,43 +859,43 @@ public class Solution {
     /**
      * 141. 环形链表
      * 给定一个链表，判断链表中是否有环。
-     *
+     * <p>
      * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
      * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
      * 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
-     *
+     * <p>
      * 如果链表中存在环，则返回 true 。 否则，返回 false 。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 进阶：
-     *
+     * <p>
      * 你能用 O(1)（即，常量）内存解决此问题吗？
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 示例 1：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 输入：head = [3,2,0,-4], pos = 1
      * 输出：true
      * 解释：链表中有一个环，其尾部连接到第二个节点。
      * 示例 2：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 输入：head = [1,2], pos = 0
      * 输出：true
      * 解释：链表中有一个环，其尾部连接到第一个节点。
      * 示例 3：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 输入：head = [1], pos = -1
      * 输出：false
      * 解释：链表中没有环。
-     *
+     * <p>
      * 哈希方法
      *
      * @param head
@@ -938,37 +938,37 @@ public class Solution {
     /**
      * 160. 相交链表
      * 编写一个程序，找到两个单链表相交的起始节点。
-     *
+     * <p>
      * 如下面的两个链表：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 在节点 c1 开始相交。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 示例 1：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
      * 输出：Reference of the node with value = 8
      * 输入解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
-     *
-     *
+     * <p>
+     * <p>
      * 示例 2：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
      * 输出：Reference of the node with value = 2
      * 输入解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
-     *
-     *
+     * <p>
+     * <p>
      * 示例 3：
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
      * 输出：null
      * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
@@ -979,9 +979,16 @@ public class Solution {
      * @return
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = (pA == null ? headB : pA.next);
+            pB = (pB == null ? headA : pB.next);
+        }
 
-
-        return null;
+        return pA;
     }
 
 
