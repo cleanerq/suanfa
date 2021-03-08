ee3bede1abab2173e4024fb8af661771ca8d22d4
qiaobuyun <qbyun130@163.com>
2021-03-08 14:14:58 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution3.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution3.java
@@ -839,6 +839,12 @@ public class Solution3 {
         return ans;
     }
 
+    /**
+     * n与n-1 做与运算
+     *
+     * @param n
+     * @return
+     */
     public static int hammingWeight3(int n) {
         int ans = 0;
 
@@ -850,6 +856,100 @@ public class Solution3 {
 
         return ans;
     }
+
+    /**
+     * 202. 快乐数
+     * 编写一个算法来判断一个数 n 是不是快乐数。
+     *
+     * 「快乐数」定义为：
+     *
+     * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+     * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+     * 如果 可以变为  1，那么这个数就是快乐数。
+     * 如果 n 是快乐数就返回 true ；不是，则返回 false 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：19
+     * 输出：true
+     * 解释：
+     * 12 + 92 = 82
+     * 82 + 22 = 68
+     * 62 + 82 = 100
+     * 12 + 02 + 02 = 1
+     * 示例 2：
+     *
+     * 输入：n = 2
+     * 输出：false
+     *
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNum(n);
+        }
+        return n == 1;
+    }
+
+    private int getNum(int n) {
+        int totalNum = 0;
+        while (n > 0) {
+            int sInt = n % 10;
+            n = n/ 10;
+            totalNum = totalNum + sInt* sInt;
+        }
+        return totalNum;
+    }
+
+    /**
+     * 快指针 慢指针 方法
+     *
+     * 快指针 一次走两步
+     * 慢指针 一次走一步
+     *
+     * @param n
+     * @return
+     */
+    public boolean isHappy2(int n) {
+        int slow = n;
+        int fast = getNum(n);
+        while (fast != 1 && slow != fast) {
+            slow = getNum(slow);
+            fast = getNum(getNum(fast));
+        }
+
+        return fast == 1;
+    }
+
+    private static Set<Integer> cycleMembers =
+            new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    /**
+     * 如果这样做，您会发现只有一个循环：4 \rightarrow 16 \rightarrow 37 \rightarrow 58 \rightarrow 89 \rightarrow 145 \rightarrow 42 \rightarrow 20 \rightarrow 44→16→37→58→89→145→42→20→4。所有其他数字都在进入这个循环的链上，或者在进入 11 的链上。
+     *
+     * 因此，我们可以硬编码一个包含这些数字的散列集，如果我们达到其中一个数字，那么我们就知道在循环中。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/happy-number/solution/kuai-le-shu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param n
+     * @return
+     */
+    public boolean isHappy3(int n) {
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNum(n);
+        }
+        return n == 1;
+    }
+
 
     public static void main(String[] args) {
 //        System.out.println((int)'A');
