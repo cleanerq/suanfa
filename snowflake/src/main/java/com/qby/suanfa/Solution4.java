ab6ed38bec4bbd697e1059b82386152ccf3b617c
qiaobuyun <qbyun130@163.com>
2021-03-11 17:06:49 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution4.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution4.java
@@ -284,18 +284,18 @@ public class Solution4 {
     /**
      * 665. 非递减数列
      * 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
-     *
+     * <p>
      * 我们是这样定义一个非递减数列的： 对于数组中任意的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: nums = [4,2,3]
      * 输出: true
      * 解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
      * 示例 2:
-     *
+     * <p>
      * 输入: nums = [4,2,1]
      * 输出: false
      * 解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
@@ -303,11 +303,41 @@ public class Solution4 {
      * @param nums
      * @return
      */
-    public boolean checkPossibility(int[] nums) {
-        boolean flg = false;
+    public static boolean checkPossibility(int[] nums) {
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                if (i == 1 || nums[i] >= nums[i - 2]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+                if (++count > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * LCP 18. 早餐组合
+     *
+     * @param staple
+     * @param drinks
+     * @param x
+     * @return
+     */
+    public int breakfastNumber(int[] staple, int[] drinks, int x) {
 
 
-        return flg;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPossibility(new int[]{3, 4, 2, 3
+        }));
     }
 
 }
