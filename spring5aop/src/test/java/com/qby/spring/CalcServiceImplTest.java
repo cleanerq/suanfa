c60ed06033de0f3fda26fad5db0e4c950f48cec8
qiaobuyun <qbyun130@163.com>
2021-03-12 17:32:49 +0800

    import

---
+++ spring5aop/src/test/java/com/qby/spring/CalcServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.qby.spring;
+
+
+import com.qby.spring.aop.CalcService;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.SpringVersion;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest  //spring5
+public class CalcServiceImplTest {
+    @Autowired
+    private CalcService service;
+
+    @Test
+    public void testAop4() {
+        System.out.println("spring版本：" + SpringVersion.getVersion() + "\t" + "SpringBoot版本：" + SpringBootVersion.getVersion());
+
+        System.out.println();
+
+        service.div(10, 2);
+    }
+
+}
+
