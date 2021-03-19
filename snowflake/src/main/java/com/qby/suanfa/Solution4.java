fa4c293d3c201d5f0ff9577979b3fb43a77f3b87
qiaobuyun <qbyun130@163.com>
2021-03-19 11:41:05 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution4.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution4.java
@@ -1395,7 +1395,7 @@ public class Solution4 {
      * <p>
      * 输入：time = "1?:22"
      * 输出："19:22"
-     *
+     * <p>
      * 贪心算法
      *
      * @param time
@@ -1420,7 +1420,79 @@ public class Solution4 {
         return stb.toString();
     }
 
+    /**
+     * 1013. 将数组分成和相等的三个部分
+     * 给你一个整数数组 A，只有可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。
+     * <p>
+     * 形式上，如果可以找出索引 i+1 < j 且满足 A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1] 就可以将数组三等分。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：[0,2,1,-6,6,-7,9,1,2,0,1]
+     * 输出：true
+     * 解释：0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+     * 示例 2：
+     * <p>
+     * 输入：[0,2,1,-6,6,7,9,-1,2,0,1]
+     * 输出：false
+     * 示例 3：
+     * <p>
+     * 输入：[3,3,6,5,-2,2,5,1,-9,4]
+     * 输出：true
+     * 解释：3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+     *
+     * @param arr
+     * @return
+     */
+    public static boolean canThreePartsEqualSum(int[] arr) {
+        int s = 0;
+        for (int num : arr) {
+            s += num;
+        }
+        if (s % 3 != 0) {
+            return false;
+        }
+        int target = s / 3;
+        int n = arr.length, i = 0, cur = 0;
+        while (i < n) {
+            cur += arr[i];
+            if (cur == target) {
+                break;
+            }
+            ++i;
+        }
+        if (cur != target) {
+            return false;
+        }
+        int j = i + 1;
+        while (j + 1 < n) {  // 需要满足最后一个数组非空
+            cur += arr[j];
+            if (cur == target * 2) {
+                return true;
+            }
+            ++j;
+        }
+        return false;
+    }
+
+    public static boolean canThreePartsEqualSum1(int[] arr) {
+        int len = arr.length - 1;
+        for (int i = 1; i <= len; ++i) {
+            arr[i] += arr[i - 1];
+        }
+
+        if (arr[len] % 3 != 0) return false;
+
+        int eqsum = arr[len] / 3, times = 1;
+        for (int i = 0; i < len && times < 3; ++i) {
+            if (arr[i] == eqsum * times) ++times;
+        }
+        return times == 3;
+    }
+
     public static void main(String[] args) {
-        System.out.println(maximumTime("?4:03"));
+        System.out.println(canThreePartsEqualSum(new int[]{1, -1, 1, -1}));
     }
 }
