e59633c3176f59e69828959767e1861c1a7807a1
qiaobuyun <qbyun130@163.com>
2021-03-19 17:24:35 +0800

    import

---
+++ redis2/src/main/java/com/qby/redis2/BootRedis01Application.java
@@ -0,0 +1,12 @@
+package com.qby.redis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BootRedis01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedis01Application.class);
+    }
+}
