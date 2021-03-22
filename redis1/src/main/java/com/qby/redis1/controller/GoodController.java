02a46094123c2929cb82a75f0a3fa835fdcf2f9d
qiaobuyun <qbyun130@163.com>
2021-03-22 16:36:12 +0800

    import

--- redis1/src/main/java/com/qby/redis1/controller/GoodController.java
+++ redis1/src/main/java/com/qby/redis1/controller/GoodController.java
@@ -6,6 +6,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @RestController
 public class GoodController {
 
@@ -15,21 +18,46 @@ public class GoodController {
     @Value("${server.port}")
     private String serverPort;
 
+    private final Lock lock = new ReentrantLock();
+
     @GetMapping("/buy_goods")
-    public String buy_Goods(){
+    public String buy_Goods() {
 
-        String result = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+        synchronized (this) {
+            String result = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodsNumber = result == null ? 0 : Integer.parseInt(result);
 
-        if (goodsNumber > 0){
-            int realNumber = goodsNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001",realNumber + "");
-            System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件"+"\t 服务器端口: "+serverPort);
-            return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件"+"\t 服务器端口: "+serverPort;
-        }else {
-            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临"+"\t 服务器端口: "+serverPort);
+            if (goodsNumber > 0) {
+                int realNumber = goodsNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+                System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+                return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+            } else {
+                System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
+            }
+            return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
         }
-        return "商品已经售罄/活动结束/调用超时，欢迎下次光临"+"\t 服务器端口: "+serverPort;
+    }
+
+    @GetMapping("/buy_goods2")
+    public String buy_Goods2() {
+        if (lock.tryLock()) {
+            try {
+                String result = stringRedisTemplate.opsForValue().get("goods:001");
+                int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+                if (goodsNumber > 0) {
+                    int realNumber = goodsNumber - 1;
+                    stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+                    System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+                    return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+                }
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
+        }
+        return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
     }
 
 }
