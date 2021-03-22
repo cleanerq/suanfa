3d84b82b15362e9010600123a9ee5825de004c42
qiaobuyun <qbyun130@163.com>
2021-03-22 17:52:39 +0800

    v8.1 使用事务

--- redis1/src/main/java/com/qby/redis1/controller/GoodController.java
+++ redis1/src/main/java/com/qby/redis1/controller/GoodController.java
@@ -6,14 +6,13 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 
 @RestController
 public class GoodController {
-
-
     public static final String REDIS_LOCK_KEY = "lockhhf";
 
     @Autowired
@@ -23,34 +22,45 @@ public class GoodController {
     private String serverPort;
 
     @GetMapping("/buy_goods")
-    public String buy_Goods(){
+    public String buy_Goods() {
 
-        String value = UUID.randomUUID().toString()+Thread.currentThread().getName();
-        try{
+        String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
+
+        try {
             //setIfAbsent() == setnx 就是如果不存在就新建，同时加上过期时间保证原子性
-            Boolean lockFlag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK_KEY, value,10L, TimeUnit.SECONDS);
-            stringRedisTemplate.expire(REDIS_LOCK_KEY,10L, TimeUnit.SECONDS);
+            Boolean lockFlag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK_KEY, value, 10L, TimeUnit.SECONDS);
+            stringRedisTemplate.expire(REDIS_LOCK_KEY, 10L, TimeUnit.SECONDS);
             if (!lockFlag) {
                 return "抢锁失败，┭┮﹏┭┮";
-            }else {
+            } else {
                 String result = stringRedisTemplate.opsForValue().get("goods:001");
                 int goodsNumber = result == null ? 0 : Integer.parseInt(result);
 
-                if (goodsNumber > 0){
+                if (goodsNumber > 0) {
                     int realNumber = goodsNumber - 1;
-                    stringRedisTemplate.opsForValue().set("goods:001",realNumber + "");
-                    System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件"+"\t 服务器端口: "+serverPort);
-
-                    return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件"+"\t 服务器端口: "+serverPort;
-                }else {
-                    System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临"+"\t 服务器端口: "+serverPort);
+                    stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+                    System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+                    return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+                } else {
+                    System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
                 }
-                return "商品已经售罄/活动结束/调用超时，欢迎下次光临"+"\t 服务器端口: "+serverPort;
+                return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
             }
-
-        }finally {
-            if (value.equalsIgnoreCase(stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY))){
-                stringRedisTemplate.delete(REDIS_LOCK_KEY);//释放锁
+        } finally {
+            while (true) {
+                stringRedisTemplate.watch(REDIS_LOCK_KEY); //加事务，乐观锁
+                if (value.equalsIgnoreCase(stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY))) {
+                    stringRedisTemplate.setEnableTransactionSupport(true);
+                    stringRedisTemplate.multi();//开始事务
+                    stringRedisTemplate.delete(REDIS_LOCK_KEY);
+                    List<Object> list = stringRedisTemplate.exec();
+                    if (list == null) {  //如果等于null，就是没有删掉，删除失败，再回去while循环那再重新执行删除
+                        continue;
+                    }
+                }
+                //如果删除成功，释放监控器，并且breank跳出当前循环
+                stringRedisTemplate.unwatch();
+                break;
             }
         }
 
--- redis2/src/main/java/com/qby/redis2/controller/GoodController.java
+++ redis2/src/main/java/com/qby/redis2/controller/GoodController.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +27,7 @@ public class GoodController {
     public String buy_Goods() {
 
         String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
+
         try {
             //setIfAbsent() == setnx 就是如果不存在就新建，同时加上过期时间保证原子性
             Boolean lockFlag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK_KEY, value, 10L, TimeUnit.SECONDS);
@@ -40,17 +42,27 @@ public class GoodController {
                     int realNumber = goodsNumber - 1;
                     stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
                     System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
-
                     return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
                 } else {
                     System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
                 }
                 return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
             }
-
         } finally {
-            if (value.equalsIgnoreCase(stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY))) {
-                stringRedisTemplate.delete(REDIS_LOCK_KEY);//释放锁
+            while (true) {
+                stringRedisTemplate.watch(REDIS_LOCK_KEY); //加事务，乐观锁
+                if (value.equalsIgnoreCase(stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY))) {
+                    stringRedisTemplate.setEnableTransactionSupport(true);
+                    stringRedisTemplate.multi();//开始事务
+                    stringRedisTemplate.delete(REDIS_LOCK_KEY);
+                    List<Object> list = stringRedisTemplate.exec();
+                    if (list == null) {  //如果等于null，就是没有删掉，删除失败，再回去while循环那再重新执行删除
+                        continue;
+                    }
+                }
+                //如果删除成功，释放监控器，并且breank跳出当前循环
+                stringRedisTemplate.unwatch();
+                break;
             }
         }
 
