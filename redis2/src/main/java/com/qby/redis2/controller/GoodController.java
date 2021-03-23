82a7da01ad7f1d478090c895ddb4e19e6cc37485
qiaobuyun <qbyun130@163.com>
2021-03-23 14:59:11 +0800

    v9 使用redission

--- redis2/src/main/java/com/qby/redis2/controller/GoodController.java
+++ redis2/src/main/java/com/qby/redis2/controller/GoodController.java
@@ -1,5 +1,7 @@
 package com.qby.redis2.controller;
 
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -27,6 +29,9 @@ public class GoodController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Autowired
+    private Redisson redisson;
 
     @GetMapping("/buy_goods")
     public String buy_Goods() throws Exception {
@@ -58,6 +63,37 @@ public class GoodController {
                     + "return redis.call('del', KEYS[1])" + "else " + "  return 0 " + "end";
             RedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
             stringRedisTemplate.execute(redisScript, Collections.singletonList(REDIS_LOCK_KEY), value);
+        }
+    }
+
+    /**
+     * 使用redission实现
+     * 来源于官网 https://redis.io/topics/distlock
+     *
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/buy_goods2")
+    public String buy_Goods2() throws Exception {
+        String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
+
+        RLock redissonLock = redisson.getLock(REDIS_LOCK_KEY);
+        redissonLock.lock();
+        try {
+            String result = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+
+            if (goodsNumber > 0) {
+                int realNumber = goodsNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+                System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+                return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+            } else {
+                System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
+            }
+            return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
+        } finally {
+            redissonLock.unlock();
         }
     }
 }
