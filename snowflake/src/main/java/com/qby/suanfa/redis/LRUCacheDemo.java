aea7a71f87e1a37c5f4f6e2c6a32b244a6c458f3
qiaobuyun <qbyun130@163.com>
2021-03-24 15:47:56 +0800

    import

---
+++ snowflake/src/main/java/com/qby/suanfa/redis/LRUCacheDemo.java
@@ -0,0 +1,45 @@
+package com.qby.suanfa.redis;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+/**
+ * 使用 jdk LinkedHashMap实现LRU
+ * @param <K>
+ * @param <V>
+ */
+public class LRUCacheDemo<K, V> extends LinkedHashMap<K, V> {
+    private int capacity;//缓存坑位
+
+    public LRUCacheDemo(int capacity) {
+        super(capacity, 0.75F, false);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return super.size() > capacity;
+    }
+
+    public static void main(String[] args) {
+        LRUCacheDemo lruCacheDemo = new LRUCacheDemo(3);
+
+        lruCacheDemo.put(1, "a");
+        lruCacheDemo.put(2, "b");
+        lruCacheDemo.put(3, "c");
+        System.out.println(lruCacheDemo.keySet());
+
+        lruCacheDemo.put(4, "d");
+        System.out.println(lruCacheDemo.keySet());
+
+        lruCacheDemo.put(3, "c");
+        System.out.println(lruCacheDemo.keySet());
+        lruCacheDemo.put(3, "c");
+        System.out.println(lruCacheDemo.keySet());
+        lruCacheDemo.put(3, "c");
+        System.out.println(lruCacheDemo.keySet());
+        lruCacheDemo.put(5, "x");
+        System.out.println(lruCacheDemo.keySet());
+    }
+}
