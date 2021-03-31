fe96940459efe151d99a556f9f45353fc0f2d3a5
qiaobuyun <qbyun130@163.com>
2021-03-31 10:38:53 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution3.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution3.java
@@ -1630,8 +1630,75 @@ public class Solution3 {
     }
 
 
+    /**
+     * 206. 反转链表
+     * 反转一个单链表。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 5->4->3->2->1->NULL
+     * <p>
+     * 指针画图解决
+     * https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+     * <p>
+     * 迭代法
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 作者：wang_ni_ma
+     * 链接：https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * 递归解法
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList2(ListNode head) {
+        //递归终止条件是当前为空，或者下一个节点为空
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //这里的cur就是最后一个节点
+        ListNode cur = reverseList2(head.next);
+        //这里请配合动画演示理解
+        //如果链表是 1->2->3->4->5，那么此时的cur就是5
+        //而head是4，head的下一个是5，下下一个是空
+        //所以head.next.next 就是5->4
+        head.next.next = head;
+        //防止链表循环，需要将head.next设置为空
+        head.next = null;
+        //每层递归函数都返回cur，也就是最后一个节点
+        return cur;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(findMaxAverage(new int[]{1, 12, -5, -6, 50, 3
-        }, 4));
+        ListNode head = new ListNode(1);
+        ListNode cur = head;
+
+        for (int i = 0; i < 4; i++) {
+            ListNode t = new ListNode(2 + i);
+            cur.next = t;
+            cur = t;
+        }
+
+        reverseList2(head);
     }
 }
