266b6b37d2a6eb184a039b187dd3ed967c4ac835
qiaobuyun <qbyun130@163.com>
2021-04-25 14:25:17 +0800

    import

--- snowflake/src/main/java/com/qby/suanfa/Solution6.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution6.java
@@ -1,9 +1,8 @@
 package com.qby.suanfa;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import com.qby.suanfa.basic.TreeNode;
+
+import java.util.*;
 
 public class Solution6 {
 
@@ -68,6 +67,8 @@ public class Solution6 {
      * 解释:
      * 对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
      * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     * <p>
+     * 方法二：栈（单调栈）
      *
      * @param nums1
      * @param nums2
@@ -93,5 +94,220 @@ public class Solution6 {
             res[i] = map.getOrDefault(nums1[i], -1);
         }
         return res;
+    }
+
+    /**
+     * 500. 键盘行
+     * 给你一个字符串数组 words ，只返回可以使用在 美式键盘 同一行的字母打印出来的单词。键盘如下图所示。
+     * <p>
+     * 美式键盘 中：
+     * <p>
+     * 第一行由字符 "qwertyuiop" 组成。
+     * 第二行由字符 "asdfghjkl" 组成。
+     * 第三行由字符 "zxcvbnm" 组成。
+     * American keyboard
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：words = ["Hello","Alaska","Dad","Peace"]
+     * 输出：["Alaska","Dad"]
+     * 示例 2：
+     * <p>
+     * 输入：words = ["omk"]
+     * 输出：[]
+     * 示例 3：
+     * <p>
+     * 输入：words = ["adsdf","sfd"]
+     * 输出：["adsdf","sfd"]
+     *
+     * @param words
+     * @return
+     */
+    public static String[] findWords(String[] words) {
+        String[] pad = new String[]{"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < pad.length; i++) {
+            char[] chars = pad[i].toCharArray();
+            for (char aChar : chars) {
+                map.put(aChar, i + 1);
+            }
+        }
+
+        String[] res = new String[words.length];
+        int j = 0;
+        int line = 0;
+        for (String word : words) {
+            char[] chars = word.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                char c = Character.toLowerCase(chars[i]);
+                Integer mline = map.get(c);
+
+                if (i == 0) {
+                    line = mline;
+                }
+
+                if (!map.containsKey(c) || line != mline.intValue()) {
+                    break;
+                }
+
+                if (i == chars.length - 1) {
+                    res[j++] = word;
+                }
+            }
+        }
+        return Arrays.copyOf(res, j);
+    }
+
+    /**
+     * 501. 二叉搜索树中的众数
+     * 给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+     * <p>
+     * 假定 BST 有如下定义：
+     * <p>
+     * 结点左子树中所含结点的值小于等于当前结点的值
+     * 结点右子树中所含结点的值大于等于当前结点的值
+     * 左子树和右子树都是二叉搜索树
+     * 例如：
+     * 给定 BST [1,null,2,2],
+     * <p>
+     * 1
+     * \
+     * 2
+     * /
+     * 2
+     * 返回[2].
+     *
+     * @param root
+     * @return
+     */
+    List<Integer> answer = new ArrayList<Integer>();
+    int base, count, maxCount;
+
+    public int[] findMode(TreeNode root) {
+        dfs(root);
+        int[] mode = new int[answer.size()];
+        for (int i = 0; i < answer.size(); ++i) {
+            mode[i] = answer.get(i);
+        }
+        return mode;
+    }
+
+    public void dfs(TreeNode o) {
+        if (o == null) {
+            return;
+        }
+        dfs(o.left);
+        update(o.val);
+        dfs(o.right);
+    }
+
+    public void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.add(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer.clear();
+            answer.add(base);
+        }
+    }
+
+    /**
+     * Morris 中序遍历的一个重要步骤就是寻找当前节点的前驱节点，并且 Morris 中序遍历寻找下一个点始终是通过转移到 \textit{right}right 指针指向的位置来完成的。
+     * <p>
+     * 如果当前节点没有左子树，则遍历这个点，然后跳转到当前节点的右子树。
+     * 如果当前节点有左子树，那么它的前驱节点一定在左子树上，我们可以在左子树上一直向右行走，找到当前点的前驱节点。
+     * 如果前驱节点没有右子树，就将前驱节点的 \textit{right}right 指针指向当前节点。这一步是为了在遍历完前驱节点后能找到前驱节点的后继，也就是当前节点。
+     * 如果前驱节点的右子树为当前节点，说明前驱节点已经被遍历过并被修改了 \textit{right}right 指针，这个时候我们重新将前驱的右孩子设置为空，遍历当前的点，然后跳转到当前节点的右子树。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-zhong-shu-by-leetcode-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param root
+     * @return
+     */
+    public int[] findMode2(TreeNode root) {
+        TreeNode cur = root, pre = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                update(cur.val);
+                cur = cur.right;
+                continue;
+            }
+            pre = cur.left;
+            while (pre.right != null && pre.right != cur) {
+                pre = pre.right;
+            }
+            if (pre.right == null) {
+                pre.right = cur;
+                cur = cur.left;
+            } else {
+                pre.right = null;
+                update(cur.val);
+                cur = cur.right;
+            }
+        }
+        int[] mode = new int[answer.size()];
+        for (int i = 0; i < answer.size(); ++i) {
+            mode[i] = answer.get(i);
+        }
+        return mode;
+    }
+
+    /**
+     * 504. 七进制数
+     * 给定一个整数，将其转化为7进制，并以字符串形式输出。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 100
+     * 输出: "202"
+     * 示例 2:
+     * <p>
+     * 输入: -7
+     * 输出: "-10"
+     *
+     * @param num
+     * @return
+     */
+    public static String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+
+        StringBuilder st = new StringBuilder();
+        boolean zs = num > 0 ? true : false;
+        int mod = 0;
+        while (num != 0) {
+            mod = num % 7;
+            st.append(Math.abs(mod));
+            num = num / 7;
+        }
+
+        if (!zs) {
+            st.append("-");
+        }
+
+        return st.reverse().toString();
+    }
+
+    public String convertToBase7_2(int num) {
+        return num >= 0 ? Integer.toUnsignedString(num, 7) : "-" + Integer.toUnsignedString(num * -1, 7);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToBase7(0));
+
+        System.out.println(Integer.toUnsignedString(10, 7));
     }
 }
