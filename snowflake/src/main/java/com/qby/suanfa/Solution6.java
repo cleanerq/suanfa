17e595536fdab7a88592c6d3689c2b60dc7c2017
qiaobuyun <qbyun130@163.com>
2021-04-25 09:19:39 +0800

    import

---
+++ snowflake/src/main/java/com/qby/suanfa/Solution6.java
@@ -0,0 +1,97 @@
+package com.qby.suanfa;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution6 {
+
+    /**
+     * 492. 构造矩形
+     * 作为一位web开发者， 懂得怎样去规划一个页面的尺寸是很重要的。 现给定一个具体的矩形页面面积，
+     * 你的任务是设计一个长度为 L 和宽度为 W 且满足以下要求的矩形的页面。要求：
+     * <p>
+     * 1. 你设计的矩形页面必须等于给定的目标面积。
+     * <p>
+     * 2. 宽度 W 不应大于长度 L，换言之，要求 L >= W 。
+     * <p>
+     * 3. 长度 L 和宽度 W 之间的差距应当尽可能小。
+     * 你需要按顺序输出你设计的页面的长度 L 和宽度 W。
+     * <p>
+     * 示例：
+     * <p>
+     * 输入: 4
+     * 输出: [2, 2]
+     * 解释: 目标面积是 4， 所有可能的构造方案有 [1,4], [2,2], [4,1]。
+     * 但是根据要求2，[1,4] 不符合要求; 根据要求3，[2,2] 比 [4,1] 更能符合要求. 所以输出长度 L 为 2， 宽度 W 为 2。
+     *
+     * @param area
+     * @return
+     */
+    public int[] constructRectangle(int area) {
+        int sqrt = (int) Math.sqrt(area);
+        if (sqrt * sqrt == area) {
+            return new int[]{sqrt, sqrt};
+        }
+        for (int i = sqrt; i >= 1; i--) {
+            if (area % i == 0) {
+                return new int[]{area / i, i};
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 496. 下一个更大元素 I
+     * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+     * <p>
+     * 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+     * <p>
+     * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。
+     * 如果不存在，对应位置输出 -1 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+     * 输出: [-1,3,-1]
+     * 解释:
+     * 对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+     * 对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+     * 对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     * 示例 2:
+     * <p>
+     * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+     * 输出: [3,-1]
+     * 解释:
+     * 对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+     * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        // 先处理 nums2，把对应关系存入哈希表
+        for (int i = 0; i < len2; i++) {
+            while (!stack.isEmpty() && stack.peekLast() < nums2[i]) {
+                map.put(stack.removeLast(), nums2[i]);
+            }
+            stack.addLast(nums2[i]);
+        }
+
+        // 遍历 nums1 得到结果集
+        int[] res = new int[len1];
+        for (int i = 0; i < len1; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
