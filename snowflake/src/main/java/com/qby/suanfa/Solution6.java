1c02a53d8f9f9196ec1ac772f99efb0492a1d431
cleanerq <qbyun130@163.com>
2021-04-27 22:58:30 +0800

    等待

--- snowflake/src/main/java/com/qby/suanfa/Solution6.java
+++ snowflake/src/main/java/com/qby/suanfa/Solution6.java
@@ -557,6 +557,86 @@ public class Solution6 {
         return Math.max(a.length(), b.length());
     }
 
+    /**
+     * 530. 二叉搜索树的最小绝对差
+     * 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+     * <p>
+     * <p>
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：
+     * <p>
+     * 1
+     * \
+     * 3
+     * /
+     * 2
+     * <p>
+     * 输出：
+     * 1
+     * <p>
+     * 解释：
+     * 最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+     *
+     * @param root
+     * @return
+     */
+    int pre;
+    int ans;
+
+    public int getMinimumDifference(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs2(root);
+        return ans;
+    }
+
+    public void dfs2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs2(root.left);
+        if (pre == -1) {
+            pre = root.val;
+        } else {
+            ans = Math.min(ans, root.val - pre);
+            pre = root.val;
+        }
+        dfs2(root.right);
+    }
+
+
+    /**
+     * 541. 反转字符串 II
+     * 给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+     * <p>
+     * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+     * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+     * <p>
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: s = "abcdefg", k = 2
+     * 输出: "bacdfeg"
+     *
+     * @param s
+     * @param k
+     * @return
+     */
+    public String reverseStr(String s, int k) {
+        char[] a = s.toCharArray();
+        for (int start = 0; start < a.length; start += 2 * k) {
+            int i = start, j = Math.min(start + k - 1, a.length - 1);
+            while (i < j) {
+                char tmp = a[i];
+                a[i++] = a[j];
+                a[j--] = tmp;
+            }
+        }
+        return new String(a);
+    }
+
     public static void main(String[] args) {
         System.out.println(detectCapitalUse("FFFFFFFFFFFFFFFFFFFFf"));
     }
