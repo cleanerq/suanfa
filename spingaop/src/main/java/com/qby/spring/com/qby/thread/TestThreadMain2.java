cc7ac20712a746b1d4460c7968e7d4cafec0d4f1
qiaobuyun <qbyun130@163.com>
2021-06-29 14:47:11 +0800

    import

--- spingaop/src/main/java/com/qby/spring/com/qby/thread/TestThreadMain2.java
+++ spingaop/src/main/java/com/qby/spring/com/qby/thread/TestThreadMain2.java
@@ -3,6 +3,7 @@ package com.qby.spring.com.qby.thread;
 public class TestThreadMain2 {
     public static void main(String[] args) {
         CustomThreadPool.threadPoolExecutor.execute(()->{
+            Thread.currentThread().setName("xxxx12111");
             System.out.println(Thread.currentThread().getName() + "xxx");
         });
     }
